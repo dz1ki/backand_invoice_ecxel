9bddedca12a119cbb3b9c4d8509b32469e1ef7b3
dz1ki <dz1kill06@gmail.com>
2024-08-28 15:25:15 +0300

    refactoring

--- web-api/src/routes/invoices.ts
+++ web-api/src/routes/invoices.ts
@@ -4,4 +4,4 @@ import InvoiceController from "../controllers/invoices";
 export const invoices: express.IRouter = express.Router();
 invoices.post("/generateInvoice", InvoiceController.generate);
 invoices.patch("/addInfoInvoice", InvoiceController.addInvoiceSatatus);
-invoices.get("/invoices/:userId/:invoiceId", InvoiceController.getInvoice);
+invoices.get("/invoice/:userId/:invoiceId", InvoiceController.getInvoice);
